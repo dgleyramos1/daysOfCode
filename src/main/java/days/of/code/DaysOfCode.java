71af29c397dc23f234087cef81d2fab634a137b6
Dgley ramos <dgleysilva@gmail.com>
2023-02-20 09:22:43 -0300

    initial: initializig project 7 days of code

---
+++ src/main/java/days/of/code/DaysOfCode.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package days.of.code;
+
+/**
+ *
+ * @author dgley
+ */
+public class DaysOfCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
